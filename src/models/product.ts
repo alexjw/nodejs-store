a8ca312d4f6dabea89e14ecb6d63c9006116c81c
Alex J <abel.oalex@gmail.com>
2020-03-26 23:56:49 -0400

    saved products on a file

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,14 +1,35 @@
+import {strict} from "assert";
+
 const products: Product[] = [];
+import Path from 'path'
+import Fs from 'fs'
 
 class Product {
     constructor(public title: string) { }
 
     save(): void {
-        products.push(this);
+        //products.push(this);
+        const p = Path.join(Path.dirname(process.mainModule.filename), 'src', 'data', 'products.json');
+        Fs.readFile(p, (err, data) => {
+            let products: Product[] = [];
+            if(!err)
+                products = JSON.parse(data.toString()) as Product[];
+            products.push(this);
+            Fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            })
+        })
     }
 
     static fetchAll(): Product[] {
-        return products;
+        const p = Path.join(Path.dirname(process.mainModule.filename), 'src', 'data', 'products.json');
+        let data: Buffer;
+        try {
+            data = Fs.readFileSync(p);
+        } catch (e) {
+            return [];
+        }
+        return JSON.parse(data.toString()) as Product[];
     }
 }
 
